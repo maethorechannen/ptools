6bad4f50c7a07bbc915a8e64c831e73dc697d044
Scot McSweeney-Roberts <git@mcsweeney-roberts.co.uk>
2009-03-01 11:47:56 +0000

    AuthPage

--- ptools.py
+++ ptools.py
@@ -4,19 +4,22 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-class MainPage(webapp.RequestHandler):
-    template_path =  os.path.join(os.path.dirname(__file__), 'templates')
+class AuthPage(webapp.RequestHandler):
     def get(self):
         user = users.get_current_user()
         if user:
-            path = os.path.join(self.template_path, 'main.html')
-            template_values = {
-                'user' : user
-            }
-            self.response.out.write(template.render(path, template_values))
-            
+            self.auth_get(user)            
         else:
             self.redirect(users.create_login_url(self.request.uri))
+
+class MainPage(AuthPage):
+    template_path =  os.path.join(os.path.dirname(__file__), 'templates')
+    def auth_get(self, user):
+        path = os.path.join(self.template_path, 'main.html')
+        template_values = {
+            'user' : user
+        }
+        self.response.out.write(template.render(path, template_values))
 
 application = webapp.WSGIApplication(
                                      [('/', MainPage)],
